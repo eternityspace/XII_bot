8af5d56d0131d919ed3d2af63342a425f9a4be14
eternityspace <143422667+eternityspace@users.noreply.github.com>
2024-02-05 17:06:11 +0200

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,177 @@
+﻿from collections import UserDict
+from datetime import datetime
+
+
+class Field:
+
+    def __init__(self, value):
+
+        if self.validator(value):
+            self.__value = value
+        else:
+            raise ValueError
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+    @staticmethod
+    def validator(value):
+        return True
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+
+        if self.validator(value):
+            self.__value = value
+        else:
+            raise ValueError
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+
+    @staticmethod
+    def validator(value):
+        return len(str(value)) == 10
+
+
+class Birthday(Field):
+    @staticmethod
+    def validator(value):
+        try:
+            datetime.strptime(value, '%d.%m.%Y')
+            return True
+        except:
+            return False
+
+
+class Record:
+    def __init__(self, name, phone=None, birthday=None):
+        self.name = Name(name)
+
+        if phone == None:
+            self.phones = []
+        else:
+            self.phones = [Phone(phone)]
+
+        self.birthday = Birthday(birthday) if birthday else None
+
+    def add_phone(self, phone):
+        phone = Phone(phone)
+        if str(phone.value) not in [str(p.value) for p in self.phones]:
+            self.phones.append(phone)
+            print('\n Phone has been added \n')
+        return self
+
+    def days_to_birthday(self):
+
+        if self.birthday:
+            day_of_birthday = datetime.strptime(
+                self.birthday.value, '%d.%m.%Y')
+            today = datetime.today()
+            data = day_of_birthday.replace(year=today.year)
+
+            delta = data - today
+            if delta.days < 0:
+                data = data.replace(year=today.year + 1)
+                delta = data - today
+
+            return delta.days
+
+        return
+
+    def edit_name(self, new_name):
+        self.name.value = Name(new_name)
+
+    def edit_phone(self, old_phone, new_phone):
+        phone = self.find_phone(old_phone)
+        self.remove_phone(phone.value)
+        self.add_phone(new_phone)
+        return self
+
+    def find_phone(self, search):
+
+        for phone in self.phones:
+            if str(phone.value) == search:
+                return phone
+
+        raise ValueError
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+        return self
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def __repr__(self):
+        return self.name.value
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        for user, data in self.data.items():
+            if str(data.name.value) == str(record.name.value):
+                return self.data[user]
+        self.data[record.name.value] = record
+
+        return record
+
+    def find(self, name: str):  # -> Record:
+        #inner method. not for user's search
+        for key, value in self.data.items():
+            if name == str(key.value):
+                return self.data[key]
+
+    def iterator(self, n=2):
+        values = list(self.data.values())
+        for i in range(0, len(values), n):
+            yield values[i:i+n]
+
+    def delete(self, name) -> str:
+        for person in self.data:
+
+            if isinstance(person, Name):
+                if str(person.value) == name:
+                    self.data.pop(person)
+                    return
+            else:
+                if person == name:
+                    self.data.pop(person)
+                    return
+        raise KeyError
+    
+    def search(self, text):
+        searched_text = text.strip().lower()
+        result = []
+        if not searched_text:
+            return result
+        for record in self.data.values():
+            if searched_text in str(record.name.value).lower() + ' '.join([phone.value for phone in record.phones]):
+                result.append(record)
+        return result
+                
+    def edit_record(self, old_name, new_name: str):
+        record = self.find(old_name)
+        if record:
+
+            self.delete(str(record.name.value))
+            record.edit_name(new_name)
+            record = self.add_record(record)
+
+        else:
+            raise KeyError
+
+        return record
+    # def __eq__(self, other):
+    #     return isinstance(other, Phone) and self.value == other.value
---
+++ main.py
@@ -0,0 +1,214 @@
+﻿from classes import *
+import pickle
+import itertools
+
+
+FILE_NAME = 'phone_book.pickle'
+
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return ('\n  There is no contact with this name!\n')
+        except ValueError:
+            return ('\n  Check the phone number! Should be 10 digits\n')
+        except IndexError:
+            return ('\n  Check your input!\n')
+
+    return inner
+
+
+@input_error
+def add(book, data):
+
+    data = data[0]
+    name = Name(data[0])
+    record = Record(name)
+    record = book.add_record(record)
+
+    if len(data) > 1:
+        phone = data[1]
+        result = record.add_phone(phone)
+        book.delete(str(record.name.value))
+        book.add_record(result)
+        return result
+
+    return record
+
+
+def console_input():
+    return input('> ').lower().strip()
+
+
+@input_error
+def edit(book, data):
+    data = data[0]
+    if len(data) == 2:
+        old_name, new_name = data
+
+        record = book.edit_record(old_name, new_name)
+        return record
+
+    elif len(data) == 3:
+        name, old_phone, new_phone = data
+        record = book.find(name)
+        record = record.edit_phone(old_phone, new_phone)
+        return record
+    else:
+        raise IndexError
+
+
+@input_error
+def find(book, data):
+
+    search = data[0]
+
+    if search.isdigit():
+        phone = search
+        for name, record in book.items():
+            if record.find_phone(phone):
+                return f'\n{record}\n'
+            else:
+                raise KeyError
+    else:
+
+        result = book.find(search)
+        if not result:
+            raise KeyError
+        return f"\n{result}\n"
+
+
+def hello(book, data):
+    return '\n  Hello how can I help you?\n'
+
+
+def help(book, data):
+    return TEXT
+
+
+def good_bye(book, data):
+    try:
+        with open(FILE_NAME, 'wb') as fh:
+            pickle.dump(book.data, fh)
+
+    except Exception as e:
+        return e
+
+    return 'Good bye!'
+
+
+@input_error
+def remove(book, data):
+    data = data[0]
+    name = data[0]
+
+    if len(data) == 1:
+        record = book.delete(name)
+        return '\n  Contact has been removed\n'
+
+    else:
+        phone = data[1]
+        record = book.find(name)
+        book.delete(record)
+        if record:
+            result = record.remove_phone(phone)
+            # book.delete(record)
+            book.add_record(result)
+            return result
+        else:
+            raise KeyError
+
+
+def search(book, data):
+    text = data[0][0]
+    result = book.search(text)
+
+    if result != []:
+        for record in result:
+            print(record)
+
+    else:
+        print('\n  No results \n')
+
+
+def show_all(book, data):
+    if not book:
+        print('\n Phone book is empty\n')
+
+    else:
+        # for name, record in book.data.items():
+        #     print(f'\n{record}\n')
+        result = book.iterator(2)
+
+        for record in next(result):
+            print(record)
+
+
+@input_error
+def parser(user_input, commands):
+    for command in commands:
+        if user_input.startswith(command):
+            data = user_input.replace(command, '').split()
+            return commands[command], data
+    else:
+        raise IndexError
+
+
+def main():
+    book = AddressBook()
+
+    commands = {
+        'add': add,
+        'close': good_bye,
+        'edit': edit,
+        'exit': good_bye,
+        'good bye': good_bye,
+        'hello': hello,
+        'help': help,
+        'search': search,
+        'remove': remove,
+        'show all': show_all,
+    }
+    try:
+        with open(FILE_NAME, 'rb') as fh:
+            read_book = pickle.load(fh)
+            book.data = read_book
+    except:
+        print('New phone book has been created\n')
+
+    while True:
+        # try:
+        user_input = console_input()
+        function, *data = parser(user_input, commands)
+        result = function(book, data)
+
+        if result is not None:
+            print(result)
+        if result == 'Good bye!':
+            break
+
+        # except:
+        #     print('\n Check your input! \n')
+
+
+if __name__ == '__main__':
+
+    TEXT = \
+        """
+                       Commands list
+        
+    create new contact   - - - > 'add name' or 'add name phone(10 digits)'
+    add phone to contact - - - > 'add name phone(10 digits)'
+    find name or phone   - - - > 'find name' or 'find phone'
+    edit contact name    - - - > 'edit old_name new_name'
+    edit conctact phone  - - - > 'edit name old_phone new_phone(10 digits)'
+    remove contact       - - - > 'remove name'
+    remove phone         - - - > 'remove name phone'
+    searching            - - - > 'search text'
+    show all phone book  - - - > 'show all'
+    commands list        - - - > 'help'
+    """
+    print(TEXT)
+    main()
